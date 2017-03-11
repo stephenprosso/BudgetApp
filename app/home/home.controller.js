a12eb1e5254fc63a95e3af4a699ed48b48fe864a
Stephen P Rosso <stephenprosso@Stephens-MacBook-Pro.local>
2017-03-11 14:04:34 -0800

    income working

--- app/home/home.controller.js
+++ app/home/home.controller.js
@@ -4,13 +4,11 @@ angular.module('BudgetApp')
 function HomeController() {
     var vm = this;
 
-    vm.test = "test text";
-    vm.list = [ ];
-    vm.click = function click() {
-        vm.newObject = {"description": vm.description, "moneyIn": vm.moneyIn};
-        vm.list.push(vm.newObject);
+    vm.incomeArray = [ ];
+    vm.incomeClick = function incomeClick() {
+        vm.newObject = {"incomeDescription": vm.incomeDescription, "moneyIn": vm.moneyIn};
+        vm.incomeArray.push(vm.newObject);
         vm.description = " ";
-        vm.moneyIn = "0";
     }
 
 }
